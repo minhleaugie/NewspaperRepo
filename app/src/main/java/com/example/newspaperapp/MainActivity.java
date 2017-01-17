32dcba6a70c6701aee251c10613e606cbc703086
Andrew <andrewbainter13@augustana.edu>
2017-01-16 21:41:42 -0600

    Added a branding toolbar

--- app/src/main/java/com/example/newspaperapp/MainActivity.java
+++ app/src/main/java/com/example/newspaperapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.newspaperapp;
 
+import android.app.ActionBar;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,7 +10,9 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -91,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
         });
         page = 0;
         pageSwitcher(5);
+
 
     }
 
