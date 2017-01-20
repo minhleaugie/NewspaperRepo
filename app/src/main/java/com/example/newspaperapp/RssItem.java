91b12a51bc67b8bde4ec825b6dc8e8232220d653
Riley Moss <Riley>
2017-01-20 11:19:45 -0600

    Added tags to RssItem (untested, but should be working)

--- app/src/main/java/com/example/newspaperapp/RssItem.java
+++ app/src/main/java/com/example/newspaperapp/RssItem.java
@@ -1,5 +1,7 @@
 package com.example.newspaperapp;
 
+import java.util.ArrayList;
+
 /**
  * Created by BrightLight on 1/4/2017.
  */
@@ -9,6 +11,11 @@ public class RssItem {
     private String description;
     private String link;
     private String date;
+    private ArrayList<String> tags;
+
+    public RssItem(){
+        tags = new ArrayList<String>();
+    }
 
     public String getDate() {
         return date;
@@ -41,4 +48,11 @@ public class RssItem {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public void addCategoryTag(String tag){
+        String trimmedTag = tag.substring(tag.indexOf("[")+1,tag.indexOf("]"));
+        tags.add(trimmedTag);
+    }
+
+    public ArrayList<String> getCategoryTags() { return tags; }
 }
