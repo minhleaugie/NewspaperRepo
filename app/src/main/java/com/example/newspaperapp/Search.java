264c9b7c3c1e9ee804116676a71625afa5a76c0c
andrewbainter13 <andrewbainter13@augustana.edu>
2017-01-23 15:43:46 -0600

    Improved responsiveness in the Search function

--- app/src/main/java/com/example/newspaperapp/Search.java
+++ app/src/main/java/com/example/newspaperapp/Search.java
@@ -3,6 +3,7 @@ package com.example.newspaperapp;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
@@ -28,7 +29,7 @@ public class Search extends AppCompatActivity {
 
     private EditText search;
     private String stringSearch = "Search . . .";
-    private List<RssItem> items;
+    private ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -54,9 +55,9 @@ public class Search extends AppCompatActivity {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if(actionId == EditorInfo.IME_ACTION_DONE){
-                    //do yo thang
                     String searchMe = buildString(search.getText().toString());
-                    makeSearchList(searchMe);
+                    new SearchListTask().execute(searchMe);
+                    dialog = new ProgressDialog(Search.this, ProgressDialog.STYLE_SPINNER).show(Search.this,"Searching", "Getting search results");
                     InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(v.getWindowToken(),0);
                     return true;
@@ -76,14 +77,22 @@ public class Search extends AppCompatActivity {
     }
 
 
-    //BAD. REDUNDANT.
-    public void makeSearchList(String link) {
-        RssParser parser = new RssParser();
+    private class SearchListTask extends AsyncTask<String, Void, List<RssItem>> {
+        protected List<RssItem> doInBackground(String[] urls) {
+            RssParser parser = new RssParser();
+            return parser.getNewsList(urls[0]);
+        }
+        protected void onPostExecute(List<RssItem> items){
+            dialog.dismiss();
+            updateList(items);
+        }
+    }
 
-        items = parser.getNewsList(link);
 
-        //create the adapter with layout from new_list_layout and the List<RssItem> items
-        NewsListAdapter adapter = new NewsListAdapter(this, R.layout.news_list_layout, items);
+
+    private void updateList(final List<RssItem> items){
+        // create the adapter with layout from new_list_layout and the List<RssItem> items
+        NewsListAdapter adapter = new NewsListAdapter(Search.this, R.layout.news_list_layout, items);
 
         ListView listView = (ListView) findViewById(R.id.search_list_view);
         listView.setAdapter(adapter);
@@ -93,9 +102,7 @@ public class Search extends AppCompatActivity {
                 Intent intent = new Intent(Search.this, ArticleActivity.class);
                 intent.putExtra(Variables.LINK, items.get(position).getLink());
                 startActivity(intent);
-
             }
         });
-
     }
 }
